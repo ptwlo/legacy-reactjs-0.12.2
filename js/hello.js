3253f0b8a759acb5d68d9ff98734bdef53abc6f4
Petter Lopez <petterlopez003@gmail.com>
2020-03-06 16:03:34 -0500

    from js to jsx

--- js/hello.js
+++ js/hello.js
@@ -1,9 +1,22 @@
+/* *************************** Without JSX Syntax ***************
 var HelloWorld = React.createClass({
   render:  function(){
-    return React.createElement('h1', null,"Hello Petter from React 0.12.2");
+    return React.createElement('h1', null,"Hello Petter Wilson from React 0.12.2");
   }
 });
 React.render(
   React.createElement(HelloWorld, null),
   document.getElementById('root')
 );
+*/
+
+// Under, with JSX Syntax.
+var HelloWorld = React.createClass({
+  render:  function(){
+    return (
+      <h1>Hello Developer</h1>,
+      <h1>From React 0.12.2</h1>
+    );
+  }
+});
+React.render(<HelloWorld />, document.getElementById('root'));
