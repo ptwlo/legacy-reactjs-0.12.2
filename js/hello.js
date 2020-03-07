c0b0957f5b49548be2184957ad132cff6b04db97
Petter Lopez <petterlopez003@gmail.com>
2020-03-06 22:30:34 -0500

    my components

--- js/hello.js
+++ js/hello.js
@@ -11,9 +11,27 @@ React.render(
 */
 
 // Under, with JSX Syntax.
-var HelloWorld = React.createClass({
-  render:  function(){
-    return (<h1>Hello</h1>);
+var Clock = React.createClass({
+  render: ()=>{
+    var time = new Date();
+    // Needs to update when time changes, next fix.
+    return <span>{time.toLocaleTimeString()}</span>;
+  }
+});
+var Author = React.createClass({
+  render: function(){
+    return <span>Petter Lopez</span>;
+  }
+});
+ var HelloWorld = React.createClass({
+  render: function(){
+    return (
+      <div id="content">
+        <div><Clock/></div>
+        <h1>React: <span id="ver">0.12.2</span> - By <Author/></h1>
+        <hr/>
+      </div>
+    );
   }
 });
 React.render(<HelloWorld />, document.getElementById('root'));
